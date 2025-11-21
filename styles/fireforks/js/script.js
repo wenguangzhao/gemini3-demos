61829feb96bb4584c64d9434047672878046a84b
wgzhao <wgzhaocv@gmail.com>
2025-11-21 23:03:47 +0900

    Remove unused font files and update CSS to eliminate dependency on "Russo One" font; adjust JavaScript to use "Gabriola" font only for improved consistency in text rendering.

--- styles/fireforks/js/script.js
+++ styles/fireforks/js/script.js
@@ -81,7 +81,7 @@ const stages = [trailsStage, mainStage];
 const randomWords = [""];
 const wordDotsMap = {};
 randomWords.forEach((word) => {
-	wordDotsMap[word] = MyMath.literalLattice(word, 3, "Gabriola,华文琥珀", "90px");
+	wordDotsMap[word] = MyMath.literalLattice(word, 3, "Gabriola", "90px");
 });
 
 // 自定义背景
@@ -1700,7 +1700,7 @@ function getWordDots(word) {
 	//随机字体大小 60~130
 	var fontSize = Math.floor(Math.random() * 70 + 60);
 
-	var res = MyMath.literalLattice(word, 3, "Gabriola,华文琥珀", fontSize + "px");
+	var res = MyMath.literalLattice(word, 3, "Gabriola", fontSize + "px");
 
 	return res;
 }
